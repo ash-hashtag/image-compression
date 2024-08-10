f1a53d6eb316f0d9cb00a58109336e26e7c0bfba
ash-hashtag <hashtag438@gmail.com>
2024-08-10 19:52:56 +0530

    dump out js files

---
+++ image_compression_bg.wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_target_free(a: number): void;
+export function target_new(a: number, b: number, c: number, d: number, e: number, f: number, g: number): number;
+export function compress_image(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
