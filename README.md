07c774ee894a065ee0b78eac9811fd26800db2d9
ash-hashtag <hashtag438@gmail.com>
2024-08-10 19:50:23 +0530

    dump js out, so npms can figure out

--- README.md
+++
@@ -1,9 +0,0 @@
-# Image Compression
-
-compatible with wasm
-
-build:
-	wasm-pack build --target web
-
-install:
-	yarn add <path/to/pkg>
--- pkg/image_compression_bg.wasm.d.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function __wbg_target_free(a: number): void;
-export function target_new(a: number, b: number, c: number, d: number, e: number, f: number, g: number): number;
-export function compress_image(a: number, b: number, c: number): void;
-export function __wbindgen_add_to_stack_pointer(a: number): number;
